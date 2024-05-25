aaf946c13e30da627bd018287d41624e28081bb7
Bogdan Putineanu <94795061+197Bogdan@users.noreply.github.com>
2024-05-25 07:22:12 +0300

    added model associations

--- models.js
+++ models.js
@@ -2,4 +2,15 @@ const User = require('./models/user');
 const Post = require('./models/post');
 const Review = require('./models/review');
 
+User.hasMany(Post);
+Post.belongsTo(User);
+
+User.hasMany(Review);
+Review.belongsTo(User);
+
+Post.hasMany(Review);
+Review.belongsTo(Post);
+
+
+
 module.exports = { User, Post, Review};
